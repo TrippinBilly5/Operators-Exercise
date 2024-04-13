993d31325dda3e3049566e015f91dc222adc2d60
mark aldridge <mark.aldridge1@protonmail.com>
2024-04-12 17:51:32 -0700

    operator exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace OperatorExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿int a = 17;
+int b = 4;
+int sum = a + b;
+
+int c = 21;
+int d = 5;
+int quotient = c / d;
+int remainder = a % b;
+
+Console.WriteLine($"{c}/{d} is {quotient} remainder {remainder}");
