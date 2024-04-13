df3dcb3e6930f87aa68992bae05218b672944d23
mark aldridge <mark.aldridge1@protonmail.com>
2024-04-12 20:46:51 -0700

    add circle area calculator

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -8,3 +8,13 @@ int quotient = c / d;
 int remainder = a % b;
 
 Console.WriteLine($"{c}/{d} is {quotient} remainder {remainder}");
+
+Console.WriteLine("input the radius of a circle");
+double radius = double.Parse(Console.ReadLine());
+double area = AreaOfCircle(radius);
+Console.WriteLine($"The area of a circle with radius of {radius} is {area}.");
+
+double AreaOfCircle( double radius)
+{
+    return Math.PI * Math.Pow(radius, 2);
+}
